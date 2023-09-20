c5d6a30ee9661e063b09193b46c5344dfcc604ab
sreejasaha19 <105979237+sreejasaha19@users.noreply.github.com>
2023-09-20 13:34:54 +0530

    Added demo_code.py

--- demo_code.py
+++ demo_code.py
@@ -1,10 +1,9 @@
-""" Basic python program to print hello world"""
+# Python program to check if the input number is odd or even.
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number.
 
-
-def greet():
-
-    """Function to print Hello world"""
-    print("Hello world")
-
-
-greet()
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print("{0} is Even".format(num))
+else:
+    print("{0} is Odd".format(num))
